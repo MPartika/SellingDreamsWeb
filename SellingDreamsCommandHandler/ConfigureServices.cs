7b0f7973ccdb466bfbe462df270b1f24eeed2ae3
komin25 <martinkominpartika@gmail.com>
2024-04-08 17:43:32 +0200

    optimise configure services

--- SellingDreamsCommandHandler/ConfigureServices.cs
+++ SellingDreamsCommandHandler/ConfigureServices.cs
@@ -16,9 +16,16 @@ public static class ConfigureServices
     {
         foreach (var type in assembly.GetTypes().Where(t => t.IsClass && !t.IsAbstract))
         {
-            foreach (var appliedInterface in type.GetInterfaces().Where(i => i.IsGenericType))
+            foreach (var appliedInterface in type.GetInterfaces().Where(i => i.IsGenericType && (
+                    i.GetGenericTypeDefinition() == typeof(ICommandHandlerAsync<,>) ||
+                    i.GetGenericTypeDefinition() == typeof(ICommandHandlerAsync<>) ||
+                    i.GetGenericTypeDefinition() == typeof(ICommandHandlerListAsync<,>) ||
+                    i.GetGenericTypeDefinition() == typeof(ICommandHandler<>) ||
+                    i.GetGenericTypeDefinition() == typeof(ICommandHandler<,>) ||
+                    i.GetGenericTypeDefinition() == typeof(ICommandHandlerList<,>)
+            )))
             {
-                    services.AddTransient(appliedInterface, type);
+                services.AddTransient(appliedInterface, type);
             }
         }
     }
